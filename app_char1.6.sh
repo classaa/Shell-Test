c139a20b8dee7791ed57ccf1c7d1b0e9c82434da
virgily <1352031300@qq.com>
2016-08-25 13:48:56 +0800

    Create app_char1.6.sh

---
+++ app_char1.6.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+## 程序名称：           app_char.sh
+## 程序版本：           v2.1
+## 程序作者：           Yang
+## 程序用法：           app_char.sh { filename } { number }
+## 程序历史：
+## 程序功能：统计文档中英文字母的个数，不区分大小写
+##
+##
+
+## Set 设置
+#set -o nounset #set -u
+#set -o errexit #set -e
+#set -o xtrace  #set -x
+
+## Program information
+PP=$(cd $(dirname $0) && pwd)
+PN=$(basename $0 ".sh")
+VER=1.0
+#TMPFILE=$PP/$PN.log
+
+## Function list
+usage(){ echo "$PN.sh  { filename } { number } " && exit;}
+
+## 主功能函数
+tongji(){
+        MIN=$2
+        MAX=$3
+        awk 'BEGIN{ FS="" } 
+        NR>'$MIN' && NR<='$MAX'{
+                for(i=1;i<=NF;i++)
+                {
+                        list[toupper($i)]++
+                }
+        }END{
+                for(j in list){
+                        if( j ~ /[A-Za-z]/)printf("%c=%d\n",j,list[j])
+                }
+        }' $1
+}
+
+## 参数判断·
+[ -f "$1" ] && FILENAME=$1 || usage
+
+[ $2 -gt 0 ] >/dev/null 2>&1 && NUM=$2 || usage
+
+#文件的总行数
+HS=$(cat $FILENAME|wc -l)
+
+## Main 
+
+for((i=0;i<$NUM;i++))
+do
+        MINHS=$[ $(($i*$HS))/$NUM ]
+        MAXHS=$[ $((($i+1)*$HS))/$NUM ]
+        tongji $FILENAME $MINHS $MAXHS >./tmpfile.$i &
+done
+wait
+awk -F"=" '{ sum[$1]=sum[$1]+$2 } 
+                END{
+                        for(j in sum){
+                                printf("%2c : %3d\n",j,sum[j])
+                        }
+                }' ./tmpfile.* |sort
+find . -name "tmpfile.*" -a -type f -exec rm -f {} \;
